2697ab91aca1b0e5ed42eb260215be0154a71955
Luka Rakocevic <95525958+LukaRakocevic@users.noreply.github.com>
2022-04-18 23:10:04 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,548 @@
+const draft = [
+	{
+		name: 'A',
+		teams: [
+			{
+				name: 'Katar',
+				rang: 1,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Ekvador',
+				rang: 2,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Senegal',
+				rang: 3,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Holandija',
+				rang: 4,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			}
+		]
+	},
+	{
+		name: 'B',
+		teams: [
+			{
+				name: 'Engleska',
+				rang: 5,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Iran',
+				rang: 6,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'SAD',
+				rang: 7,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Ukrajina',
+				rang: 8,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			}
+		]
+	},
+	{
+		name: 'C',
+		teams: [
+			{
+				name: 'Argentina',
+				rang: 9,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Saudijska Arabija',
+				rang: 10,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Meksiko',
+				rang: 11,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Poljska',
+				rang: 12,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			}
+		]
+	},
+	{
+		name: 'D',
+		teams: [
+			{
+				name: 'Francuska',
+				rang: 13,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Peru',
+				rang: 14,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Danska',
+				rang: 15,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Tunis',
+				rang: 16,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			}
+		]
+	},
+	{
+		name: 'E',
+		teams: [
+			{
+				name: 'Spanija',
+				rang: 17,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Novi Zeland',
+				rang: 18,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Nemacka',
+				rang: 19,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Japan',
+				rang: 20,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			}
+		]
+	},
+	{
+		name: 'F',
+		teams: [
+			{
+				name: 'Belgija',
+				rang: 21,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Kanada',
+				rang: 22,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Maroko',
+				rang: 23,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Hrvatska',
+				rang: 24,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			}
+		]
+	},
+	{
+		name: 'G',
+		teams: [
+			{
+				name: 'Brazil',
+				rang: 25,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Srbija',
+				rang: 26,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Svajcarska',
+				rang: 27,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Kamerun',
+				rang: 28,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			}
+		]
+	},
+	{
+		name: 'H',
+		teams: [
+			{
+				name: 'Portugal',
+				rang: 29,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Gana',
+				rang: 30,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Urugvaj',
+				rang: 31,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			},
+			{
+				name: 'Juzna Koreja',
+				rang: 32,
+				points: 0,
+				scored: 0,
+				taken: 0,
+				won: 0,
+				lost: 0,
+				tied: 0
+			}
+		]
+	}
+];
+
+const SCORE_PERCENT = 1; // rang diff percent (if lower than SCORE_PERCENT, tied)
+const MAX_SCORE = 4; // let's keep it realistic...
+
+const SEPARATOR = '---------------------------------------------------------';
+
+/**
+ * Game play function
+ * @param teamA
+ * @param teamB
+ * @param log
+ * @param eliminations
+ * @returns {boolean|*[]}
+ */
+const playGame = (teamA, teamB, log = false, eliminations = false) => {
+	let percent = parseInt(teamA.rang*SCORE_PERCENT/100)+1;
+	let rangDiff = Math.abs(teamA.rang - teamB.rang);
+	let teamAScored = 0;
+	let teamBScored = 0;
+	let winner = false;
+	// in eliminations, there's no tie
+	if(rangDiff <= percent && !eliminations) {
+		// tied match
+		let randScore = Math.floor(Math.random() * MAX_SCORE) + 1;
+		teamA.points += 1;
+		teamB.points += 1;
+		
+		teamA.scored += randScore;
+		teamA.taken += randScore;
+
+		teamB.scored += randScore;
+		teamB.taken += randScore;
+		teamA.tied += 1;
+		teamB.tied += 1;
+		teamAScored = randScore;
+		teamBScored = randScore
+		
+	} else {
+		let randScoreA = Math.floor(Math.random() * MAX_SCORE) + 1;
+		let randScoreB = Math.floor(Math.random() * MAX_SCORE) + 1;
+		
+		if(teamA.rang > teamB.rang) {
+			teamA.points += 3;
+			teamA.scored += Math.max(randScoreA, randScoreB);
+			teamA.taken += Math.min(randScoreA, randScoreB);
+
+			teamB.scored += Math.min(randScoreA, randScoreB);
+			teamB.taken += Math.max(randScoreA, randScoreB);
+
+			teamA.won += 1;
+			teamB.lost += 1;
+
+			teamAScored = Math.max(randScoreA, randScoreB);
+			teamBScored = Math.min(randScoreA, randScoreB);
+			winner = teamA;
+		} else {
+			teamB.points += 3;
+			teamB.scored += Math.max(randScoreA, randScoreB);
+			teamB.taken += Math.min(randScoreA, randScoreB);
+
+			teamA.scored += Math.min(randScoreA, randScoreB);
+			teamA.taken += Math.max(randScoreA, randScoreB);
+
+			teamB.won += 1;
+			teamA.lost += 1;
+
+			teamAScored = Math.min(randScoreA, randScoreB);
+			teamBScored = Math.max(randScoreA, randScoreB);
+			winner = teamB;
+		}
+	}
+	if(log) {
+		console.log('\t\t'+teamA.name + ' ' + teamAScored + ':' + teamBScored + ' ' + teamB.name)
+	}
+	if(eliminations) {
+		return winner;
+	}
+	return [teamA, teamB]
+
+}
+
+/**
+ * Group stage loop
+ * @param group
+ * @returns {*}
+ */
+const groupStageResults = group => {
+	let matchesPlayed = [];
+	let results = []
+	group.teams.forEach((teamA, indexA) => {
+		group.teams.forEach((teamB, indexB) => {
+			if(indexA != indexB) {
+				let matchName = teamA.name + ' - ' + teamB.name;
+				let matchNameB = teamB.name + ' - ' + teamA.name;
+
+				if(!matchesPlayed.includes(matchName) && !matchesPlayed.includes(matchNameB)) {
+					matchesPlayed.push(matchName);
+					let result = playGame(teamA, teamB, true);
+					group.teams[indexA] = result[0];
+					group.teams[indexB] = result[1];
+					results.push(result);
+				}
+			}
+		});
+	});
+
+	return group;
+}
+
+/**
+ * Group stage main loop
+ * @param groups
+ * @returns {*[]}
+ */
+const groupStage = groups => {
+	let groupStageWinners = [];
+	console.log('Grupna faza - I kolo: ');
+	groups.forEach(group => {
+		console.log('\tGrupa ' + group.name);
+		let gsr = groupStageResults(group);
+
+		console.log('Kraj grupne faze '+group.name+':');
+		let rbr = 1;
+		gsr.teams.forEach(team => {
+			console.log('\t\t'+rbr + '. ' + team.name + ' ('+team.rang+')' + ' ' + team.won + ' '
+				+ team.tied + ' ' + team.lost+ ' ' +team.scored+ ':' +team.taken + ' ' + team.points);
+			rbr++;
+		})
+
+		gsr.teams.sort((a, b) => {
+			if(b.points !== a.points) {
+				return b.points > a.points
+			}else {
+				return (b.scored - b.taken) > (a.scored - a.taken)
+			}
+
+		})
+		groupStageWinners.push([gsr.teams[0], gsr.teams[1]])
+		console.log(SEPARATOR)
+	});
+
+	return groupStageWinners;
+}
+
+/**
+ * Eliminations recursive function
+ * @param groupStageWinners
+ */
+const eliminations = groupStageWinners => {
+	console.log('Eliminaciona faza - 1/'+groupStageWinners.length*2+' finala:')
+	let winners = []
+	groupStageWinners.forEach(teams => {
+		let elWinners = playGame(teams[0], teams[1], true, true)
+		winners.push(elWinners)
+	})
+
+	if(winners.length > 1) {
+		let pairs = [];
+		winners.forEach((team, index) => {
+			if(index % 2 == 0) {
+				pairs.push([winners[index], winners[index+1]])
+			}
+		})
+
+		eliminations(pairs)
+	} else {
+		console.log("POBEDNIK:\n" +'!!! '+ winners[0].name+' !!!' )
+	}
+
+}
+
+/**
+ * Program entry point
+ * @param groups
+ */
+const main = groups => {
+	// start group stage
+	let groupStageWinners = groupStage(groups);
+
+	// pass group stage winners to eliminations
+	eliminations(groupStageWinners);
+};
+
+
+// run program
+main(draft);
